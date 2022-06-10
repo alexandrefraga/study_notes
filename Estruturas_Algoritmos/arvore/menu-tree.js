bc25a99375cafe47f74572151ffaa5359114d16c
Alexandre Fraga <alexandrenfraga@yahoo.com.br>
2022-06-10 16:45:28 -0300

    refactor menu tree

--- Estruturas_Algoritmos/arvore/menu-tree.js
+++ Estruturas_Algoritmos/arvore/menu-tree.js
@@ -1,22 +1,4 @@
-const accounts = [
-    { id: 3, parent: 1, sequence: '1' , account: 'Circulante', type: 'sintetica'},
-    { id: 4, parent: 3, sequence: '1' , account: 'Caixa', type: 'analitica'},
-    { id: 2, parent: null , sequence: '2' , account: 'Passivo', type: 'sintetica'},
-    { id: 1, parent: null, sequence: '1' , account: 'Ativo', type: 'sintetica'},
-    { id: 5, parent: 3, sequence: '2' , account: 'Banco', type: 'sintetica'},
-    { id: 7, parent: 1, sequence: '2' , account: 'Imobilizado', type: 'sintetica'},
-    { id: 10, parent: 7, sequence: '2', account: 'Imóveis', type: 'analitica'},
-    { id: 9, parent: 7, sequence: '1', account: 'Móveis', type: 'analitica'},
-    { id: 8, parent: 2, sequence: '2' , account: 'Fornenedores', type: 'sintetica'},
-    { id: 6, parent: 2, sequence: '1' , account: 'Impostos', type: 'sintetica'},
-    { id: 11, parent: 2, sequence: '3' , account: 'Patrimonio Liquido', type: 'sintetica'},
-    { id: 12, parent: 11, sequence: '1' , account: 'Lucro Acumulado', type: 'analitica'},
-    { id: 13, parent: null, sequence: '3' , account: 'Resultado', type: 'sintetica'},
-    { id: 14, parent: 13, sequence: '1' , account: 'Receitas', type: 'analitica'},
-    { id: 15, parent: 13, sequence: '25' , account: 'Despesas', type: 'analitica'},
-    { id: 16, parent: 5, sequence: '1' , account: 'Banrisul', type: 'analitica'},
-    { id: 17, parent: 5, sequence: '2' , account: 'Itau', type: 'analitica'},
-];
+const { accounts } = require('./menu.data');
 
 function addPath(account, i, arrayData = []) {
     if(account.parent) {
@@ -81,16 +63,4 @@ var inicio = performance.now();
 const inOrderAccountsWithPath = run(accounts, addPath, 100);
 console.log("Tempo de execução: ",performance.now() - inicio);
 
-inOrderAccountsWithPath.forEach(e => console.log(`${e.path} ${e.account}`));
-
-
-
-
-// accountWithPath.forEach(e => insert(tree, e))
-// const listaOrdenada = inOrder(tree);
-// listaOrdenada.forEach(e => console.log(`${e.path} ${e.account}`));
-
-// accountWithPath.sort(function(a,b) {
-//     return a.path < b.path ? -1 : a.path > b.path ? 1 : 0;
-// });
-// accountWithPath.forEach(e => console.log(`${e.path} ${e.account}`));
+inOrderAccountsWithPath.forEach((e, i) => console.log(`${i + 1}) ${e.path} ${e.account}`))
